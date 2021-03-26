4e668f8d904daf7501d36fec17acad773582652b
Garrett Rector <80281078+GarrettRector@users.noreply.github.com>
2021-03-26 14:26:26 -0400

    Added extensive exception handling
    Program will not stop just tell you to try again.

--- Dilations.py
+++ Dilations.py
@@ -1,27 +1,58 @@
 
 def main():
     # x coord
-    xp = input("X Coord : ")
-    x = int(xp)
-    # y coord
-    yp = input("Y Coord : ")
-    y = int(yp)
-    # dilation factor
-    dp = input("Dilation Factor : ")
-    d2 = str(dp)
-    try:
-        d = float(dp)
-    except:
-        one, two = d2.split("/")
-        one1 = int(one)
-        two2 = int(two)
-        d = one1 / two2
-    # x for center of dilation
-    op1 = input("Dilation center for X : ")
-    o1 = int(op1)
-    # y for center of dilation
-    op2 = input("Dilation center for Y : ")
-    o2 = int(op2)
+    while True:
+        try:
+            xp = input("X Coord : ")
+            x = int(xp)
+            break
+        except:
+            print("Incorrect Type. Try again")
+            print(" ")
+    while True:
+        try:
+            # y coord
+            yp = input("Y Coord : ")
+            y = int(yp)
+            break
+        except:
+            print("Incorrect Type. Try again")
+            print(" ")
+    while True:
+        try:
+            # dilation factor
+            dp = input("Dilation Factor : ")
+            d2 = str(dp)
+            try:
+                d = float(dp)
+                break
+            except:
+                one, two = d2.split("/")
+                one1 = int(one)
+                two2 = int(two)
+                d = one1 / two2
+                break
+        except:
+            print("Incorrect Type. Try again")
+            print(" ")
+    while True:
+        try:
+            # x for center of dilation
+            op1 = input("Dilation center for X : ")
+            o1 = int(op1)
+            break
+        except:
+            print("Incorrect Type. Try again")
+            print(" ")
+    while True:
+        try:
+            # y for center of dilation
+            op2 = input("Dilation center for Y : ")
+            o2 = int(op2)
+            break
+        except:
+            print("Incorrect Type. Try again")
+            print(" ")
 
     xs1 = x - o1
     ys1 = y - o2
