60c1d79ce876e24ab0f50c01b0708b23dc51ab26
Garrett Rector <garrettrector329@gmail.com>
2021-04-15 09:30:01 -0400

    Update Trigonometry.py

--- math 8th/Trigonometry.py
+++ math 8th/Trigonometry.py
@@ -1,4 +1,3 @@
-import numpy
 import math
 
 
@@ -7,22 +6,21 @@ def main():
     typeOf, Type, angle = inp.split(" ")
     angle = float(angle)
     if (typeOf == "cos") & (Type == "rad"):
-        print(f"cos{angle} = "'%.10s' % math.cos(angle))
+        print(f"cos {angle} = "'%.10s' % math.cos(angle))
     elif (typeOf == "sin") & (Type == "rad"):
-        print(f"sin{angle} = "'%.10s' % math.sin(angle))
+        print(f"sin {angle} = "'%.10s' % math.sin(angle))
     elif (typeOf == "tan") & (Type == "rad"):
-        print(f"tan '%.10s'{angle} = " + '%.10s' % math.tan(angle))
-    elif typeOf == "radical":
-        inp = input("What side do you have? ")
-        if inp == "short":
-            print("short")
+        print(f"tan {angle} = " + '%.10s' % math.tan(angle))
+    elif typeOf == "side":
+        inp = input("What side do you need? ")
+
     elif (typeOf == "cos") & (Type == "deg"):
-        print(f"cos ({angle}) = " + numpy.degrees(numpy.cos(angle)))
+        print(f"cos ({angle}) = {math.cos(math.radians(angle))}")
     elif (typeOf == "sin") & (Type == "deg"):
-        print(f"sin ({angle}) = " + numpy.degrees(numpy.sin(angle)))
+        print(f"sin ({angle}) = {math.sin(math.radians(angle))}")
     elif (typeOf == "tan") & (Type == "deg"):
-        print(f"tan({angle}) = " + numpy.degrees(numpy.tan(angle)))
-        
-        
+        print(f"tan ({angle}) = {math.tan(math.radians(angle))}")
+
+
 while True:
     main()
