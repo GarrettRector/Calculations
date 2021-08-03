387fec2898b8bfa89183ef618e1e857281fe24d3
GarrettRector <garrett@grector.dev>
2021-08-02 20:38:24 -0400

    Improved Coinflip programming

--- fun/Coinflip.py
+++ fun/Coinflip.py
@@ -1,62 +1,48 @@
 import random
 
 
-def Average(lst):
-    return sum(lst) / len(lst)
-
-
 def cmd():
     global times, inp, amt, chance
     while True:
+        print("Options:\n* flip\n* avg\n* luck")
         inp = input()
         try:
-            cmd, times, amt = inp.split(" ")
+            command, times, amt = inp.split(" ")
             break
         except ValueError:
             try:
-                cmd, amt = inp.split(" ")
+                command, amt = inp.split(" ")
                 break
             except ValueError:
-                cmd = inp
                 times = 1
                 amt = 1
                 break
-    if cmd == "flip":
+    if "flip" in inp:
         chance_head = 0
         chance_tail = 0
-        for i in range(int(amt)):
-            check = 0
+        for _ in range(int(amt)):
             ranum = random.randint(0, 1)
             if ranum == 0:
                 chance_head += 1
             elif ranum == 1:
                 chance_tail += 1
-            if (chance_head | chance_tail == int(amt) / 2) & (check == 0):
-                print("Halfway done!")
-                check = 1
-        print(f"Chance for Heads: {chance_head}/{amt} ({round(chance_head / int(amt) * 100, 3)}%)")
-        print(f"Chance for Tails: {chance_tail}/{amt} ({round(chance_tail / int(amt) * 100, 3)}%)")
-    elif cmd == "avg":
+        print(f"Chance for Heads: {chance_head}/{amt} ({round(chance_head / int(amt) * 100, 3)}%)\nChance for Tails: "
+              f"{chance_tail}/{amt} ({round(chance_tail / int(amt) * 100, 3)}%)")
+    elif "avg" in inp:
         listh = []
         listt = []
-        for i in range(int(times)):
-            chance_head = 0
-            chance_tail = 0
-            for i in range(int(amt)):
+        for _ in range(int(times)):
+            chance_head, chance_tail = 0, 0
+            for _ in range(int(amt)):
                 ranum = random.randint(0, 1)
-                if ranum == 0:
-                    chance_head += 1
-                elif ranum == 1:
-                    chance_tail += 1
+                chance_head += 1 if ranum == 0 else chance_tail + 1
             listh.append(chance_head / int(amt))
             listt.append(chance_tail / int(amt))
-        average = Average(listh)
-        avg = Average(listt)
-        print(f"Average chance for heads:", round(average * 100, 4))
-        print(f"Average chance for tails:", round(avg * 100, 4))
-        listh.clear()
-        listt.clear()
-    elif cmd == "luck":
+        average = sum(listh)/len(listh)
+        avg = sum(listt)/len(listt)
+        print(f"Average chance for heads: {round(average * 100, 4)}\nAverage chance for tails: {round(avg * 100, 4)}")
+        listh.clear(), listt.clear()
+    elif "luck" in inp:
         left = int(times)
         chancelist = []
         chances = 0
@@ -82,7 +68,10 @@ def cmd():
         chance, hold = str(chance).split("]")
         print(chance)
         chancelist.clear()
+    else:
+        print("Invalid input")
+        cmd()
 
 
-while True:
+if __name__ == '__main__':
     cmd()
