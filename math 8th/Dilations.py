ff34459eb4448b6c570cf511a8331d336a5c32ee
GarrettRector <garrett@grector.dev>
2021-08-02 20:56:03 -0400

    Improved Dilations

--- math 8th/Dilations.py
+++ math 8th/Dilations.py
@@ -5,61 +5,8 @@ def truncate(f, n):
     i, p, d = s.partition('.')
     return '.'.join([i, (d+'0'*n)[:n]])
 
-def main():
-    # x coord
-    while True:
-        try:
-            xp = input("X Coord : ")
-            x = float(xp)
-            break
-        except:
-            print("Incorrect Type. Try again")
-            print(" ")
-    while True:
-        try:
-            # y coord
-            yp = input("Y Coord : ")
-            y = float(yp)
-            break
-        except:
-            print("Incorrect Type. Try again")
-            print(" ")
-    while True:
-        try:
-            # dilation factor
-            dp = input("Dilation Factor : ")
-            d2 = str(dp)
-            try:
-                d = float(dp)
-                break
-            except:
-                one, two = d2.split("/")
-                one1 = int(one)
-                two2 = int(two)
-                d = one1 / two2
-                break
-        except:
-            print("Incorrect Type. Try again")
-            print(" ")
-    while True:
-        try:
-            # x for center of dilation
-            op1 = input("Dilation center for X : ")
-            o1 = float(op1)
-            break
-        except:
-            print("Incorrect Type. Try again")
-            print(" ")
-    while True:
-        try:
-            # y for center of dilation
-            op2 = input("Dilation center for Y : ")
-            o2 = float(op2)
-            break
-        except:
-            print("Incorrect Type. Try again")
-            print(" ")
 
+def logic(x, y, o1, o2, d):
     xs1 = x - o1
     ys1 = y - o2
     xs2 = xs1 * d
@@ -70,15 +17,75 @@ def main():
     ys4 = str(abs(ys4))
     xs4 = int(abs(xs3))
     xs4 = str(abs(xs4))
-    xs5 = truncate(xs4, 4+len(xs4))
-    ys5 = truncate(ys4, 4+len(ys4))
+    xs5 = truncate(xs4, 4 + len(xs4))
+    ys5 = truncate(ys4, 4 + len(ys4))
+    return xs5, ys5
+
+
+
+def main():
+    # x coord
+    while True:
+        try:
+            xp = input("X Coord : ")
+            x = float(xp)
+            break
+        except ValueError:
+            print("Incorrect Type. Try again")
+            print(" ")
+    while True:
+        try:
+            # y coord
+            yp = input("Y Coord : ")
+            y = float(yp)
+            break
+        except ValueError:
+            print("Incorrect Type. Try again")
+            print(" ")
+    while True:
+        try:
+            # dilation factor
+            dp = input("Dilation Factor : ")
+            d2 = str(dp)
+            try:
+                d = float(dp)
+                break
+            except ValueError:
+                one, two = d2.split("/")
+                one1 = int(one)
+                two2 = int(two)
+                d = one1 / two2
+                break
+        except ValueError:
+            print("Incorrect Type. Try again")
+            print(" ")
+    while True:
+        try:
+            # x for center of dilation
+            op1 = input("Dilation center for X : ")
+            o1 = float(op1)
+            break
+        except ValueError:
+            print("Incorrect Type. Try again")
+            print(" ")
+    while True:
+        try:
+            # y for center of dilation
+            op2 = input("Dilation center for Y : ")
+            o2 = float(op2)
+            break
+        except ValueError:
+            print("Incorrect Type. Try again")
+            print(" ")
+
+    xs5, ys5 = logic(x, y, o1, o2, d)
     print("Answer:")
     if "00" in xs5 or ys5:
         print(f"({int(float(xs5))}, {int(float(ys5))})")
     else:
         print(f"({xs5}, {ys5})")
-    print("")
+    print("\n")
 
 
-while True:
+if __name__ == '__main__':
     main()
