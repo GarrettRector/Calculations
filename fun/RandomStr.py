e96c51a4d50f53e437b71a8b338058382ea58ae2
GarrettRector <garrett@grector.dev>
2021-08-08 16:51:01 -0400

    Random String rework

--- fun/RandomStr.py
+++ fun/RandomStr.py
@@ -1,74 +1,75 @@
 import random
-import time
 
 
-def random_string_generator(str_size, allowed_chars):
-    return ''.join(random.choice(allowed_chars) for x in range(str_size))
+def str_gen(str_size, allowed_chars):
+    return ''.join(random.choice(allowed_chars) for _ in range(str_size))
+
 
 usedstr = []
 fstrings = []
 
-inp = input("Gui? ")
-if inp == "gui":
-    fstring = ""
-    for _ in range(10):
-        for i in range(15):
-            for n in range(2 ** (i + 1)):
-                randstring = random_string_generator(i + 1, ("A", "B"))
-                if randstring not in usedstr:
-                    print(randstring)
-                    usedstr.append(randstring)
-                else:
-                    n = n - 1
-        for _ in range(10):
-            for i in range(10):
-                string = usedstr[(i * i) ** 2]
-                try:
-                    strings = string[i]
-                except IndexError:
-                    pass
-                print(strings)
-                fstring = fstring + strings
-            print(fstring)
-            fstrings.append(fstring)
+
+def main():
+    while True:
+        inp = input("Gui? ")
+        if inp.lower() == "gui":
             fstring = ""
-            count = usedstr.count(fstring)
-            print(f"String is only in the list {count} time")
-    print("Done!")
-    time.sleep(1)
-    print("Generating new strings!")
-    time.sleep(.5)
-    fstring = ""
-    for i in range(10):
-        print(fstrings[i])
-elif inp == "no gui":
-    fstring = ""
-    for _ in range(10):
-        for i in range(15):
-            for n in range(2 ** (i + 1)):
-                randstring = random_string_generator(i + 1, ("A", "B"))
-                if randstring not in usedstr:
-                    usedstr.append(randstring)
-                else:
-                    n = n - 1
-        for _ in range(10):
+            for _ in range(10):
+                for i in range(15):
+                    for _ in range(2 ** (i + 1)):
+                        randstring = str_gen(i + 1, ("A", "B"))
+                        if randstring not in usedstr:
+                            print(randstring)
+                            usedstr.append(randstring)
+                        else:
+                            _ -= 1
+                for _ in range(10):
+                    for i in range(10):
+                        string = usedstr[(i * i) ** 2]
+                        try:
+                            strings = string[i]
+                            print(strings)
+                            fstring = fstring + strings
+                        except IndexError:
+                            print("err")
+                    print(fstring)
+                    fstrings.append(fstring)
+                    fstring = ""
+                    count = usedstr.count(fstring)
+                    print(f"String is only in the list {count} time")
+            print("Done!")
+            print("Generating new strings!")
             for i in range(10):
-                string = usedstr[(i * i) ** 2]
-                try:
-                    strings = string[i]
-                except IndexError:
-                    pass
-                print(strings)
-                fstring = fstring + strings
-            print(fstring)
-            fstrings.append(fstring)
+                print(fstrings[i])
+        else:
             fstring = ""
-            count = usedstr.count(fstring)
-            print(f"String is only in the list {count} time")
-    print("Done!")
-    time.sleep(1)
-    print("Generating new strings!")
-    time.sleep(.5)
-    fstring = ""
-    for i in range(10):
-        print(fstrings[i])
+            for _ in range(10):
+                for i in range(15):
+                    for n in range(2 ** (i + 1)):
+                        randstring = str_gen(i + 1, ("A", "B"))
+                        if randstring not in usedstr:
+                            usedstr.append(randstring)
+                        else:
+                            n = n - 1
+                for _ in range(10):
+                    for i in range(10):
+                        string = usedstr[(i * i) ** 2]
+                        try:
+                            strings = string[i]
+                            print(strings)
+                            fstring = fstring + strings
+                        except IndexError:
+                            pass
+                    print(fstring)
+                    fstrings.append(fstring)
+                    fstring = ""
+                    count = usedstr.count(fstring)
+                    print(f"String is only in the list {count} time")
+            print("Done!")
+            print("Generating new strings!")
+            for i in range(10):
+                print(fstrings[i])
+
+
+if __name__ == '__main__':
+    main()
