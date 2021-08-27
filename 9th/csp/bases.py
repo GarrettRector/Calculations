6658498034becdd0986e46296f14a8d76bcbd1c9
GarrettRector <garrett@grector.dev>
2021-08-26 22:47:58 -0400

    Able to convert bases 2-10 to any base

--- 9th/csp/bases.py
+++ 9th/csp/bases.py
@@ -1,28 +1,46 @@
-def main():
-    inp = input("To or From Base 10 > ")
-    match inp.lower():
-        case "to":
-            ans = 0
-            while True:
-                base = input("Converting from? ")
-                number = input("Number? ")
-                try:
-                    base = int(base)
-                    break
-                except ValueError:
-                    print(f'Either the base or the number is not a Integer. Try again')
-            for i in range(0, len(number)):
-                num = str(number)[i]
-                n = len(number) - (i+1)
-                pt1 = base ** n
-                answer = int(num) * int(pt1)
-                ans += int(answer)
-            print(ans)
+from string import ascii_lowercase
 
-        case "from":
-            pass
-        case _:
-            print("Unknown command")
+
+def main():
+    while True:
+        base = input("Converting from? ")
+        tobase = input("To what base? ")
+        try:
+            base = int(base)
+            break
+        except ValueError:
+            print(f'Either the base or the number is not a Integer. Try again')
+    number = input("Number? ")
+    if tobase == "10":
+        answer = to_ten(base, number)
+        print(answer)
+    else:
+        base_ten_val = to_ten(base, number)
+        answer = from_ten(tobase, base_ten_val)
+        print(answer)
+
+
+def to_ten(base, number):
+    ans = 0
+    numbers = {letter: str(index) for index, letter in enumerate(ascii_lowercase, start=10)}
+    for i in range(0, len(number)):
+        num = number[i]
+        if num.isalpha():
+            num = [numbers.get(character) for character in num.lower()]
+            num = str(num)[2:-2]
+        n = len(str(number)) - (i + 1)
+        pt1 = base ** n
+        answer = int(num) * int(pt1)
+        ans += int(answer)
+    return ans
+
+
+def from_ten(base, number):
+    ans = ""
+    while number > 0:
+        ans += str(number % int(base))
+        number = int(number/int(base))
+    return ans[::-1]
 
 
 if __name__ == '__main__':
