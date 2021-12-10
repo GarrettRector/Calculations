f225c588c9e5b32ed7db909fb975d7a49b155838
Garrett Rector <garrett@grector.dev>
2021-12-09 20:57:00 -0500

    2 lines

--- fun/Tacos.py
+++ fun/Tacos.py
@@ -1,3 +1,2 @@
 for i, _ in enumerate(iter(bool, True)):
-    inp = input("Taco? > ")
-    if "taco" in inp: print(["tortilla", "cheese", "meat", "MORE MEAT"][i]) if i <= 3 else print(arr[3])
+    if "taco" in input("Taco? > "): print(["tortilla", "cheese", "meat", "MORE MEAT"][i]) if i <= 3 else print(arr[3])
