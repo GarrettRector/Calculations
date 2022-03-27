2c3c4bb64a0478c249da10b49244316916b19168
Garrett Rector <garrett@grector.dev>
2022-03-27 16:26:52 -0400

    Just In Time (JIT) compilation added

--- fun/Mandelbrot Set.py
+++ fun/Mandelbrot Set.py
@@ -1,31 +1,28 @@
-from numpy import complex, array
+from numba import jit
 from PIL import Image
-import colorsys
-
-width_of_the_set = 20000
 
 
-def color(i):
-    color_of_the_set = 255 * array(colorsys.hsv_to_rgb(i / 245.0, 0.9, 0.4))
-    return tuple(color_of_the_set.astype(int))
-
-
-def mandelbrot_set(j, k):
+@jit(nopython=True, parallel=True)
+def mandelbrot_set(j, k) -> int:
     c1 = complex(j, k)
     c2 = 0
     for n in range(1, 900):
         if abs(c2) > 2:
-            return color(n)
+            return int(n)
         c2 = c2 ** 2 + c1
-    return 0, 0, 0
+    return 0
 
 
-image = Image.new('RGB', (width_of_the_set, int(width_of_the_set / 2)))
-pixels = image.load()
-for x in range(image.size[0]):
-    print("%.2f %%" % (x / width_of_the_set * 100.0))
-    for y in range(image.size[1]):
-        pixels[x, y] = mandelbrot_set((x - (0.75 * width_of_the_set)) / (width_of_the_set / 4),
-                                      (y - (width_of_the_set / 4)) / (width_of_the_set / 4))
-image.show()
-image.save('Mandelbrot Set.png')
+def generate(width):
+    image = Image.new('RGB', (width, int(width / 2)))
+    pixels = image.load()
+    for x in range(image.size[0]):
+        print("%.2f %%" % (x / width * 100.0))
+        for y in range(image.size[1]):
+            pixels[x, y] = mandelbrot_set((x - (0.75 * width)) / (width / 4),
+                                          (y - (width / 4)) / (width / 4))
+    image.save(f"Mandelbrot Set {width}.png")
+
+
+w = 8000 # Width of image
+generate(w)
